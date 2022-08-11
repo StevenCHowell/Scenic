4a589f52415fbdfb36f40fa32bf17252ea0ff2b2
Steven C. Howell <9484229+StevenCHowell@users.noreply.github.com>
2022-08-11 17:51:40 -0400

    get path to map.npz

--- src/scenic/simulators/gta/map.py
+++ src/scenic/simulators/gta/map.py
@@ -1,11 +1,14 @@
-
 # stub to allow changing the map without having to alter gta_model.sc
+import pathlib
 
-import os
+module_path = pathlib.Path(__file__)
+scenic_path = module_path.parent.parent.parent.parent.parent
+mapPath = str(list(scenic_path.rglob("map.npz"))[0])
 
-mapPath = 'map.npz'
 
 def setLocalMap(module, relpath):
+    import os
+
     global mapPath
     base = os.path.dirname(module)
     mapPath = os.path.join(base, relpath)
